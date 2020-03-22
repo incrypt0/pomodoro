c5295aa37b73c8d2ed85010eb3be38c7f3681522
krishna-incrypto <vpkrishnanand@gmail.com>
2020-03-22 13:10:53 +0530

    initial commit

---
+++ pomodoro.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+
+echo "__________incrypt0 Pomodoro__________"
+while [ true ]; do
+    sleep 1600;
+    zenity --warning --title="Time for a break" --text="Baby... Its time to take a break" --width=400
+done
+
